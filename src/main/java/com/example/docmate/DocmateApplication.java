dc1b0e35235529aa32e2b133dfc2d571f05bcc37
Aniket Mandal <104608457+Aniket2mandal@users.noreply.github.com>
2026-02-24 08:27:52 +0545

    first commit

---
+++ src/main/java/com/example/docmate/DocmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.docmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmateApplication.class, args);
+	}
+
+}
