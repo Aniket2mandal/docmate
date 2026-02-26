d16986cf659364402edb2496abfbab9181dffcd2
Aniket Mandal <104608457+Aniket2mandal@users.noreply.github.com>
2026-02-26 18:53:29 +0545

    mapperconfig doctor,patient entity gender status enum global respose and exception created

---
+++ src/main/java/com/example/docmate/enums/UserStatus.java
@@ -0,0 +1,5 @@
+package com.example.docmate.enums;
+
+public enum UserStatus {
+    ACTIVE,INACTIVE,SUSPENDED,DELETED
+}
---
+++ src/main/java/com/example/docmate/global/response/GlobalResponse.java
@@ -0,0 +1,20 @@
+package com.example.docmate.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class GlobalResponse {
+    private boolean status;
+    private String message;
+    private Object data;
+}
