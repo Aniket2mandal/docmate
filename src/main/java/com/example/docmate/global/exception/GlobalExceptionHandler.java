fee4b6dc8f4805350a7d0d3da34ecb156604ad5f
Aniket Mandal <104608457+Aniket2mandal@users.noreply.github.com>
2026-02-28 21:30:52 +0545

    added general exception handler

--- src/main/java/com/example/docmate/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/docmate/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.docmate.global.response.GlobalResponse;
 import com.example.docmate.global.response.GlobalResponseBuilder;
 import org.apache.logging.log4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<GlobalResponse> handleGlobalException(GlobalException e) {
         GlobalResponse response = GlobalResponseBuilder.buildErrorResponse(e);
         return ResponseEntity.status(e.getHttpStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GlobalResponse> handleException(Exception e) {
+        GlobalResponse response = GlobalResponseBuilder.buildErrorResponse(
+                new GlobalException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR)
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(response);
     }
 }
