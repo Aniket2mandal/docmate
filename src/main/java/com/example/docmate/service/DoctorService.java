06541769c5dbc575c1a68e782552e0dc06100422
Linda subedi <anilbabusubedi@Linda-MacBook-Pro.local>
2026-03-06 22:34:56 +0545

    Doctor get by id and change status end point created

--- src/main/java/com/example/docmate/service/DoctorService.java
+++ src/main/java/com/example/docmate/service/DoctorService.java
@@ -1,11 +1,16 @@
 package com.example.docmate.service;
 
-import com.example.docmate.entity.DoctorEntity;
+import com.example.docmate.enums.UserStatus;
 import com.example.docmate.global.response.GlobalResponse;
 import com.example.docmate.payload.request.DoctorRequest;
+import com.example.docmate.payload.request.UserRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 public interface DoctorService {
     GlobalResponse createDoctor(DoctorRequest doctor);
     GlobalResponse getAllDoctor();
+    GlobalResponse getDoctorById(String id);
+    GlobalResponse changeStatus(UserRequest user, String id);
 }
