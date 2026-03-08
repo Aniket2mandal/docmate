b4fd8e25728b3be202ac897aa449008069f83868
Linda subedi <anilbabusubedi@Linda-MacBook-Pro.local>
2026-03-08 23:05:56 +0545

    Doctor schedule get all schedule endpoint created

--- src/main/java/com/example/docmate/service/DoctorService.java
+++ src/main/java/com/example/docmate/service/DoctorService.java
@@ -6,7 +6,6 @@ import com.example.docmate.payload.request.DoctorScheduleRequest;
 import org.springframework.data.domain.Pageable;
 
 
-
 public interface DoctorService {
     GlobalResponse createDoctor(DoctorRequest doctor);
 
@@ -17,4 +16,5 @@ public interface DoctorService {
     GlobalResponse updateDoctor(DoctorRequest doctorRequest, String doctorId);
 
     GlobalResponse createDoctorSchedule(DoctorScheduleRequest scheduleRequest);
+    GlobalResponse getAllSchedule(String doctorId);
 }
