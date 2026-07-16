e215ac91d6635b46a028a805ee7e7f382db4e0d2
Aniket Mandal <104608457+Aniket2mandal@users.noreply.github.com>
2026-07-16 22:03:04 +0545

    added logs and xone

--- src/main/java/com/example/docmate/scheduler/AppointmentStatusScheduler.java
+++ src/main/java/com/example/docmate/scheduler/AppointmentStatusScheduler.java
@@ -5,13 +5,17 @@ import com.example.docmate.enums.AppointmentStatus;
 import com.example.docmate.enums.ScheduleAvailabilityStatus;
 import com.example.docmate.repository.AppointmentRepository;
 import com.example.docmate.repository.DoctorScheduleRepository;
+import com.example.docmate.service.impl.AppointmentServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @Component
@@ -21,11 +25,17 @@ public class AppointmentStatusScheduler {
     private final AppointmentRepository appointmentRepository;
     private final DoctorScheduleRepository doctorScheduleRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(AppointmentServiceImpl.class);
+
     @Scheduled(fixedRate = 60000)
     public void updateAppointmentStatus() {
 
-        LocalDate nowDate= LocalDate.now();
-        LocalTime nowTime= LocalTime.now();
+        log.info("Appointment scheduler running at {}", LocalDateTime.now());
+
+        ZoneId zone = ZoneId.of("Asia/Kathmandu");
+
+        LocalDate nowDate = LocalDate.now(zone);
+        LocalTime nowTime = LocalTime.now(zone);
 
         List<AppointmentEntity> appointmentEntityList = appointmentRepository.findPreviousAppointments(
                 nowDate,nowTime, AppointmentStatus.BOOKED);
@@ -41,8 +51,12 @@ public class AppointmentStatusScheduler {
     @Scheduled(fixedRate = 60000)
     public void updateScheduleStatus() {
 
-        LocalDate nowDate= LocalDate.now();
-        LocalTime nowTime= LocalTime.now();
+        log.info("Schedule scheduler running at {}", LocalDateTime.now());
+
+        ZoneId zone = ZoneId.of("Asia/Kathmandu");
+
+        LocalDate nowDate = LocalDate.now(zone);
+        LocalTime nowTime = LocalTime.now(zone);
 
         List<DoctorScheduleEntity> scheduleEntityList = doctorScheduleRepository.findPreviousSchedules(
                 nowDate,nowTime, ScheduleAvailabilityStatus.AVAILABLE);
