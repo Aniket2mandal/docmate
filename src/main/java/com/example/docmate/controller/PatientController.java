c877cc8a11edaa52abcae00c79cb7e90104bb5dd
Aniket Mandal <104608457+Aniket2mandal@users.noreply.github.com>
2026-09-10 15:54:20 +0545

    fixed mapping

--- src/main/java/com/example/docmate/controller/PatientController.java
+++ src/main/java/com/example/docmate/controller/PatientController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class PatientController {
         return ResponseEntity.ok(patientService.rateDoctor(ratingRequest));
     }
 
-    @PostMapping("/update-patient-profile/{patientId}")
+    @PutMapping("/update-patient-profile/{patientId}")
     public ResponseEntity<GlobalResponse> updatePatientProfile(@PathVariable String patientId,
                                                                @RequestBody PatientRequest patientRequest) {
         return ResponseEntity.ok(patientService.updatePatientProfile(patientId, patientRequest));
